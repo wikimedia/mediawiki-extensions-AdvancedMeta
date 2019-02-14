36654ecd4e5320454deb06d91688a67cf35df35b
Peter Boehm <boehm@hallowelt.com>
2019-02-14 10:31:30 +0100

    Fix overwriting of noindex policy
    Only mess with IndexPolicy on action=view
    
    Needs Cherrypicking to REL 1_31
    
    Change-Id: Ie0bc9368128d835bf3affbed2fae3048edafab22

--- src/Hook/BeforePageDisplay/AddPolicies.php
+++ src/Hook/BeforePageDisplay/AddPolicies.php
@@ -11,6 +11,9 @@ class AddPolicies extends BeforePageDisplay {
 
 		$request = $this->out->getContext()->getRequest();
 
+		$action = $request->getVal( 'action', 'view' );
+		$printable = $request->getVal( 'printable', 'no' );
+
 		$current = true;
 		$namespace = $this->out->getTitle()->getNamespace();
 		if ( $namespace >= 0 && $request->getIntOrNull( 'oldid' ) !== null ) {
@@ -20,13 +23,16 @@ class AddPolicies extends BeforePageDisplay {
 		if ( $noOldVersions && !$current ) {
 			$this->out->setIndexPolicy( 'noindex' );
 			$this->out->setFollowPolicy( 'nofollow' );
-		} else {
+		} elseif ( $action === 'view' && $printable === 'no' ) {
 			$this->out->setIndexPolicy(
 				$metaData[MetaHandler::INDEX] ? 'index' : 'noindex'
 			);
 			$this->out->setFollowPolicy(
 				$metaData[MetaHandler::FOLLOW] ? 'follow' : 'nofollow'
 			);
+		} else {
+			// set noindex on edit, printview etc
+			$this->out->setIndexPolicy( 'noindex' );
 		}
 		return true;
 	}
